d5e58b61dbe7d45af928ea4f99a20ba658b99040
kvncodes <kkevint0008@gmaile.com>
2025-08-06 17:05:03 +0200

    Mostly Done

---
+++ index.js
@@ -0,0 +1,66 @@
+const p1 = {
+	score: 0,
+	display: document.querySelector("#player-one-score"),
+	button: document.querySelector("#add-score-player-one"),
+};
+const p2 = {
+	score: 0,
+	display: document.querySelector("#player-two-score"),
+	button: document.querySelector("#add-score-player-two"),
+};
+
+const resetButton = document.querySelector("#reset");
+
+const input = document.querySelector("#score");
+
+let isGameOver = false;
+
+let winningScore;
+
+input.addEventListener("change", function () {
+	winningScore = parseInt(this.value);
+});
+
+p1.button.addEventListener("click", function () {
+	if (!isGameOver && winningScore) {
+		p1.score++;
+		p1.display.textContent = p1.score;
+		if (p1.score === winningScore) {
+			updateStyles(p1, p2);
+		}
+	}
+});
+
+p2.button.addEventListener("click", function () {
+	if (!isGameOver && winningScore) {
+		p2.score++;
+		p2.display.textContent = p2.score;
+		if (p2.score === winningScore) {
+			updateStyles(p2, p1);
+		}
+	}
+});
+
+resetButton.addEventListener("click", function () {
+	p1.score = 0;
+	p2.score = 0;
+	p1.display.textContent = p1.score;
+	p2.display.textContent = p2.score;
+	isGameOver = false;
+	p1.display.style.color = "black";
+	p2.display.style.color = "black";
+	p1.button.disabled = false;
+	p2.button.disabled = false;
+	p1.button.style.opacity = "1";
+	p2.button.style.opacity = "1";
+});
+
+function updateStyles(winner, loser) {
+	winner.display.style.color = "rgb(38, 165, 38)";
+	loser.display.style.color = "rgb(231, 42, 42)";
+	isGameOver = true;
+	winner.button.disabled = true;
+	loser.button.disabled = true;
+	winner.button.style.opacity = "0.5";
+	loser.button.style.opacity = "0.5";
+}
